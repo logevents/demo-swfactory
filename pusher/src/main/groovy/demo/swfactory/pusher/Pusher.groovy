ed804478e83ae50bfe31ea121eacb8304da1ac6a
Marius Keppler <Marius.Keppler@continental-corporation.com>
2021-04-09 16:24:46 +0200

    Finalized pusher

--- pusher/src/main/groovy/demo/swfactory/pusher/Pusher.groovy
+++ pusher/src/main/groovy/demo/swfactory/pusher/Pusher.groovy
@@ -1,14 +1,20 @@
 package demo.swfactory.pusher
 
-
+import com.fasterxml.jackson.databind.ObjectMapper
 import demo.swfactory.model.CollectedBuild
 import demo.swfactory.pusher.generation.CollectedBuildGeneration
+import org.apache.hc.client5.http.classic.methods.HttpGet
+import org.apache.hc.client5.http.classic.methods.HttpPost
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient
+import org.apache.hc.client5.http.impl.classic.HttpClients
+import org.apache.hc.core5.http.HttpEntity
+import org.apache.hc.core5.http.io.entity.StringEntity
 
 class Pusher {
     private String target
+    private ObjectMapper mapper = new ObjectMapper()
 
     static void main(String[] args) {
-
         new Pusher("localhost:9092").push()
     }
 
@@ -17,9 +23,22 @@ class Pusher {
     }
 
     void push() {
-
         CollectedBuild collectedBuild = CollectedBuildGeneration.generateEasyBuild()
+        send(collectedBuild)
+    }
 
-        System.out.println("Created "+collectedBuild.toString())
+    private void send(Object objectToSend) {
+        CloseableHttpClient client = HttpClients.createDefault()
+        try {
+            HttpPost request = new HttpPost(target);
+            request.setEntity(new StringEntity(toJson(objectToSend)))
+            client.execute(request)
+        } finally {
+            client.close()
+        }
+    }
+
+    String toJson(Object obj) {
+        return mapper.writeValueAsString(obj)
     }
 }
